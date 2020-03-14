cb211ba3e1031e2231a4e7a0a5146e76374ef5f5
Tyler Hueter <tylerhueter08@gmail.com>
2020-03-14 19:20:39 -0400

    swr project fetch

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,19 @@
 import Layout from '../components/Layout';
+import fetch from 'unfetch';
+import useSWR from 'swr';
+
+const API_URL = 'https://api.github.com';
+async function fetcher(path) {
+    const res = await fetch(API_URL + path);
+    const json = await res.json();
+    return json
+}; 
 
 const Projects = () => {
+    const { data, error } = useSWR('/repos/zeit/next.js', fetcher);
+
+    if (error) return <div>Failed to load.</div>;
+    if (!data) return <div>loading...</div>
     
     return (
         <Layout>
